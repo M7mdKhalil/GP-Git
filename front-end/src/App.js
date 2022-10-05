17df42c41f38a58147ed33873a15fda8154b509b
Mohammad Khalil <moody.khalil511@gmail.com>
2022-10-05 17:34:53 +0300

    styling home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,18 +4,24 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
 import Card from "./components/Card";
+import SideBar from "./components/SideBar";
 
 function App() {
   const [data, setData] = useState(null);
+
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+
+
   return (
     <div className="App">
       <Navbar />
       <div className="main-content">
+        <SideBar/>
+
         <Container>
           <Card />
           <Card />
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import classes from "../stylesheets/Navbar.module.css";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   const [scrollPos, setScrollPos] = useState(0);
@@ -17,9 +18,18 @@ const Navbar = () => {
 
   return (
     <div className={classes.header}>
-      <nav className={(scrollPos < 50) ? classes.nav : classes.navScroll}>
+      <nav className={scrollPos < 50 ? classes.nav : classes.navScroll}>
         <h1>HireHub</h1>
       </nav>
+      <div className={scrollPos < 50 && classes.bar}>
+        <div className={scrollPos > 50 && classes.search}>
+          <SearchBar />
+        </div>
+        <div className={scrollPos < 50 ? classes.log : classes.scrollLog}>
+          <a className={classes.loginButton}>Login</a>
+          <a className={classes.registerButton}>Register</a>
+        </div>
+      </div>
 
       {/* <h3>{scrollPos}</h3> */}
     </div>
