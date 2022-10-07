e0c335bc1e8cae688fc66efcd94c54d96efe4a62
bara eyad <barrad11112222@gmail.com>
2022-10-07 23:15:39 +0200

    add routes and login&register pages

--- front-end/src/components/Input.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "../stylesheets/Input.module.css";
-
-const Input = (props) => {
-    return <input className={classes.textInput} />
-}
-
-export default Input;
