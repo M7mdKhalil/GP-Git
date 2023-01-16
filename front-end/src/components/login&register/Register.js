f58a36b3d81c6e54b1c79415f40a0918415d1575
bara eyad <barrad11112222@gmail.com>
2023-01-17 00:34:37 +0200

    add simple validation

--- front-end/src/components/login&register/Register.js
+++ front-end/src/components/login&register/Register.js
@@ -22,8 +22,6 @@ import AutoCompleteInput from "../UI/AutoCompleteInput";
 import AddChip from "../UI/AddChip";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import DoneAllRoundedIcon from "@mui/icons-material/DoneRounded";
-import IconButton from "@mui/material/IconButton";
-import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import Avatar from "@mui/material/Avatar";
 import AddIcon from "@mui/icons-material/Add";
 
@@ -38,7 +36,7 @@ const Register = (props) => {
   );
   const [stateMsg, setstateMsg] = useState("");
   const [handleError, setHandleError] = useState(false);
-  const [username, setusername] = useState("");
+    const [username, setusername] = useState("");
   const [password, setpassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [email, setemail] = useState("");
@@ -48,6 +46,7 @@ const Register = (props) => {
     country: undefined,
     skill: [],
   });
+    const [errors, setErrors] = useState([{}]);
   const [imageDone, setimageDone] = useState(false);
   const [phonenumber, setphonenumber] = useState("");
   const [location, setlocation] = useState("");
@@ -114,6 +113,22 @@ const Register = (props) => {
     setHandleError(false);
   }, [username]);
 
+    const validate = () => {
+        setErrors([]);
+        if (username.length < 7 || username.length > 20) { setErrors(oldArray => [...oldArray, { name: 'username', text: 'Must be between 7 and 20 character' }]) }
+        if (!(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email))) { setErrors(oldArray => [...oldArray, { name: 'email', text: 'Not valid Email' }]) }
+        if (!(/[a-zA-Z]/g.test(password))) { setErrors(oldArray => [...oldArray, { name: 'password', text: 'Must contain at least one letter' }]) }
+        if (password.length < 8) { setErrors(oldArray => [...oldArray, { name: 'password', text: 'Must be more than 7 character' }]) }
+        if (password !== confirmPassword) { setErrors(oldArray => [...oldArray, { name: 'confirmPassword', text: 'Not equals to password' }]) }
+    }
+
+    const checkErrors = (name) => {
+        for (let i = 0; i < errors.length; i++) {
+            if (errors[i].name === name) {
+                return errors[i].text;
+            }
+        }
+    }
   return (
     <>
       {islogin ? (
@@ -125,7 +140,13 @@ const Register = (props) => {
               currentStep={currentStepHandler}
               SubmitButton={
                 <PrimaryButton
-                  onClick={submitHandler}
+                      onClick={(e) => {
+                          validate()
+                          if (errors.length===0) {
+                              submitHandler(e)
+                          }
+                          else { setStepNow(0) }
+                      }}
                   endIcon={<DoneAllRoundedIcon />}
                 >
                   finish & SignUp
@@ -135,24 +156,27 @@ const Register = (props) => {
               <div className="info">
                 {stepNow == 0 || stepNow == -1 ? (
                   <div>
-                    {handleError ? (
-                      <TextField
-                        error
-                        helperText="username is already exist!"
-                        label="username"
-                        value={username}
+                                          {handleError ? (
+                                              <TextField
+                                                  error
+                                                  helperText="username is already exist!"
+                                                  label="username"
+                                                  value={username}
+                                                  sx={{ width: 800 }}
+                                                  onChange={(e) => {
+                                                      setusername(e.target.value);
+                                                  }}
+                                              ></TextField>
+                                          ) : (
+                                              <TextField
+                                                  hel
+                                                  label="username"
+                                                      value={username}
+                                                      error={checkErrors('username') ? true : false}
+                                                      helperText={checkErrors('username')}
                         sx={{ width: 800 }}
-                        onChange={(e) => {
-                          setusername(e.target.value);
-                        }}
-                      ></TextField>
-                    ) : (
-                      <TextField
-                        label="username"
-                        value={username}
-                        sx={{ width: 800 }}
-                        placeholder="*choose unique username"
-                        onChange={(e) => {
+                                                      placeholder="*choose unique username"
+                                                      onChange={(e) => {
                           setusername(e.target.value);
                         }}
                       ></TextField>
@@ -161,7 +185,9 @@ const Register = (props) => {
                     <TextField
                       label="Email"
                       type="email"
-                      value={email}
+                                              value={email}
+                                              error={checkErrors('email') ? true : false}
+                                              helperText={checkErrors('email')}
                       sx={{ width: 800 }}
                       placeholder="example@gmail.com"
                       onChange={(e) => {
@@ -171,7 +197,9 @@ const Register = (props) => {
 
                     <TextField
                       label="Password"
-                      type="password"
+                                              type="password"
+                                              error={checkErrors('password') ? true : false}
+                                              helperText={checkErrors('password')}
                       value={password}
                       sx={{ width: 800 }}
                       onChange={(e) => {
@@ -182,7 +210,9 @@ const Register = (props) => {
                     <TextField
                       label="Confirm password"
                       type="password"
-                      value={confirmPassword}
+                                              value={confirmPassword}
+                                              error={checkErrors('confirmPassword') ? true : false}
+                                              helperText={checkErrors('confirmPassword')}
                       sx={{ width: 800 }}
                       onChange={(e) => setConfirmPassword(e.target.value)}
                     ></TextField>
